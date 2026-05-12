f823cda83114e94173a027ecda00f7eb4055b19f
Jonas <jonasvh39@gmail.com>
2026-05-12 11:20:54 +0200

    chore: bump version to 1.11.16

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,5 +1,5 @@
 // Auto-generated during build
-export const APP_VERSION = '1.11.15';
-export const APP_BUILD_COMMIT = '1496a21';
+export const APP_VERSION = '1.11.16';
+export const APP_BUILD_COMMIT = 'd7e40be';
 export const APP_BUILD_REF = 'main';
-export const APP_VERSION_DISPLAY = 'v1.11.15';
+export const APP_VERSION_DISPLAY = 'v1.11.16';
