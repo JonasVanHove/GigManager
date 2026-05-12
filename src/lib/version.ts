a5802f1601db1094e7580d84caa3e6656db21ebb
Jonas <jonasvh39@gmail.com>
2026-05-12 11:10:56 +0200

    chore: bump version to 1.11.15

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,4 +1,5 @@
-import packageJson from "../../package.json";
-
-export const APP_VERSION = packageJson.version;
-export const APP_VERSION_DISPLAY = `v${APP_VERSION}`;
+// Auto-generated during build
+export const APP_VERSION = '1.11.15';
+export const APP_BUILD_COMMIT = '1496a21';
+export const APP_BUILD_REF = 'main';
+export const APP_VERSION_DISPLAY = 'v1.11.15';
