c2cc5976024ecbb284e14fbd4a837e30ce766ee9
Jonas <jonasvh39@gmail.com>
2026-09-08 17:24:20 +0200

    1.28.18

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,5 +1,5 @@
 // Auto-generated during build
-export const APP_VERSION = '1.28.17';
-export const APP_BUILD_COMMIT = '4d2c240';
+export const APP_VERSION = '1.28.18';
+export const APP_BUILD_COMMIT = '87755c1';
 export const APP_BUILD_REF = 'main';
-export const APP_VERSION_DISPLAY = 'v1.28.17';
+export const APP_VERSION_DISPLAY = 'v1.28.18';
