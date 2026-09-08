44d318245be4f7d71a3e9a1ce15033a482e8af8d
Jonas <jonasvh39@gmail.com>
2026-09-08 22:14:17 +0200

    chore: bump version to 1.28.22

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,5 +1,5 @@
 // Auto-generated during build
-export const APP_VERSION = '1.28.21';
-export const APP_BUILD_COMMIT = '072828f';
+export const APP_VERSION = '1.28.22';
+export const APP_BUILD_COMMIT = '04f3bce';
 export const APP_BUILD_REF = 'main';
-export const APP_VERSION_DISPLAY = 'v1.28.21';
+export const APP_VERSION_DISPLAY = 'v1.28.22';
